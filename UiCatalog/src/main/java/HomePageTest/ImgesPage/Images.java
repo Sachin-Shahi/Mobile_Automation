998a908d034f83df851570652b88f47f6a80056f
Sachin-Shahi <schn.jachin@gmail.com>
2017-02-03 12:58:49 -0500

    Fixed Folder Structure, New Common Method, WIP IOS

--- UiCatalog/src/main/java/HomePageTest/ImgesPage/Images.java
+++ UiCatalog/src/main/java/HomePageTest/ImgesPage/Images.java
@@ -1,7 +1,22 @@
 package HomePageTest.ImgesPage;
 
+import common.Base;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
 /**
  * Created by mrahman on 1/15/17.
  */
-public class Images {
+public class Images extends Base {
+
+    @FindBy(xpath = "//UIAApplication[1]/UIAWindow[2]/UIASlider[1]")
+    public WebElement slider;
+
+
+    public void TouchAction() {
+        slider.sendKeys(".9");
+    }
 }
+
+
--- UiCatalog/src/main/java/HomePageTest/ToolbarPage/Toolbar.java
+++ UiCatalog/src/main/java/HomePageTest/ToolbarPage/Toolbar.java
@@ -1,7 +1,46 @@
 package HomePageTest.ToolbarPage;
 
+import HomePageTest.WebPage.Web;
+import common.Base;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
 /**
  * Created by mrahman on 1/15/17.
  */
-public class Toolbar {
+public class Toolbar extends Base {
+    @FindBy(xpath = "//UIAApplication[1]/UIAWindow[2]/UIAScrollView[1]/UIASwitch[1]")
+    public WebElement switchImage;
+
+    @FindBy(xpath = "//UIAApplication[1]/UIAWindow[2]/UIAScrollView[1]/UIASwitch[2]")
+    public WebElement switchTinted;
+
+    @FindBy(xpath = "//UIAApplication[1]/UIAWindow[2]/UIAScrollView[1]/UIAPicker[1]/UIAPickerWheel[1]")
+    public WebElement pickerWheel;
+
+
+    public void SwitchImageButtons() throws InterruptedException {
+        switchImage.getText();
+        switchImage.click();
+        sleep(3);
+        switchImage.getText();
+        switchImage.click();
+    }
+
+    public void SwitchTintedButtons() throws InterruptedException {
+        switchTinted.getText();
+        switchTinted.click();
+        sleep(3);
+        switchTinted.getText();
+        switchTinted.click();
+    }
+
+    public void PickerWheel() throws InterruptedException {
+        String[] picker = {"Done"};
+        pickerWheel.sendKeys("Cancel");
+        sleep(5);
+        scrollKeys(ad, picker, "//UIAPicker[1]");
+        sleep(5);
+    }
+
 }
